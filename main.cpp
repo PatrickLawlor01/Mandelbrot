65959d58e289503db20b8728155b27f1952b97fe
Patrick Lawlor <patrick.lawlor@fundingcircle.com>
2022-05-25 18:03:37 +0100

    test change

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 	Complex z1(12, 3), z2(-4, -5), z3;
 
 	ZMatrix* zplane = new ZMatrix();
-	zplane->setElement(500, z1);
+	zplane->setElement(500, z1.re, z1.im);
 	z3 = zplane->getElement(500);
 
 
@@ -26,11 +26,13 @@ int main(int argc, char* argv[]) {
 	cons.cout(z1.toString());
 	cons.cout(z2.toString());
 
-	//z3 = z1 * z2;
+	z3 = z1 * z2;
 	cons.cout(z3.toString());
 
 	delete zplane;
 
 	return app.exec();
+
+	//test change
 
 }
